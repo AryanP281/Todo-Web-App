69ff62a49caed732705c5fc443959c027ad91a4a
aryan <aryanpathare281@gmail.com>
2021-07-10 21:11:54 +0530

    Made todo cards draggable and sortable

--- src/Controllers/ListController.ts
+++ src/Controllers/ListController.ts
@@ -27,11 +27,12 @@ async function getUserTodos(req : Request, resp : Response) : Promise<void>
         });
 
         //Getting the todo cards
-        const todoCardDocs : any[] = await (await userCollection.find({lastIndex : {$exists: false}})).toArray();
+        const todoCardDocs : any[] = await (await userCollection.find({lastIndex : {$exists: false}}).sort({order : 1})).toArray();
         //Renaming the _id property to id
         todoCardDocs.forEach((todoCard) => {
             todoCard["id"] = todoCard._id.toString();
             delete todoCard._id;
+            delete todoCard.order;
         });
 
         //Creating the response object
@@ -67,10 +68,10 @@ async function createNewList(req : Request, resp : Response) : Promise<void>
 
         //Adding the new list name to the lists doc
         const updateObj : any = {};
-        updateObj[newList.name] = {code: listsDoc.lastIndex + 1};
+        updateObj[newList.name] = {code: Number.parseInt(listsDoc.lastIndex) + 1, cards : 0};
         await userCollection.updateOne({lastIndex : {$exists: true}}, {$set : updateObj, $inc : {lastIndex : 1}});
 
-        resp.status(200).json({success: true});
+        resp.status(200).json({success: true, code : updateObj[newList.name].code});
     }
     catch(err)
     {
@@ -126,10 +127,30 @@ async function addNewTodo(req : Request, resp : Response) : Promise<void>
     try
     {
         //Getting the todo object
-        const todoItem : {name : string, dscr : string, listCode : number} = req.body.todo;
+        const todoItem : {name : string, dscr : string, listCode : number, order : number | undefined} = req.body.todo;
 
         //Getting the user collection
         const userCollection : Collection = await mongodb.db().collection(req.body.userId);
+
+        //Getting the list documnet
+        const listsDoc = await userCollection.findOne({lastIndex : {$exists : true}}, {projection: {_id:0, lastIndex:0}});
+
+        //Getting the order of the todo item
+        const lists : [string, any][] = Object.entries(listsDoc);
+        for(let i = 0; i < lists.length; ++i)
+        {
+            if(lists[i][1].code === todoItem.listCode)
+            {
+                todoItem.order = lists[i][1].cards;
+
+                //Updating the list card count
+                const incObj : any = {};
+                incObj[`${lists[i][0]}.cards`] = 1;
+                await userCollection.updateOne({lastIndex : {$exists : true}}, {$inc : incObj});
+
+                break;
+            }
+        }
 
         //Writing the todo object
         const todoDoc = await userCollection.insertOne(todoItem);
@@ -152,6 +173,25 @@ async function deleteTodo(req : Request, resp : Response) : Promise<void>
         //Getting the user collection
         const userCollection : Collection = await mongodb.db().collection(req.body.userId);
 
+        //Getting the todo item document
+        const todoDocument = await userCollection.findOne({_id : new ObjectId(req.body.todoId)});
+
+        //Updating the orders of the remaining documents
+        await userCollection.updateMany({$and : [{listCode : todoDocument.listCode}, {order : {$gt : todoDocument.order}}]}, {$inc : {order : -1}});
+
+        //Updating the list cards count
+        const lists : [string, any][] = Object.entries(await userCollection.findOne({lastIndex : {$exists : true}}, {projection: {_id:0, lastIndex:0}}) as any);
+        for(let i = 0; i < lists.length; ++i)
+        {
+            if(lists[i][1].code === todoDocument.listCode)
+            {
+                const incObj : any = {};
+                incObj[`${lists[i][0]}.cards`] = -1;
+                await userCollection.updateOne({lastIndex : {$exists : true}}, {$inc : incObj});
+                break;
+            }
+        }
+
         //Deleting the todo item document
         await userCollection.deleteOne({_id : new ObjectId(req.body.todoId)});
 
@@ -164,5 +204,91 @@ async function deleteTodo(req : Request, resp : Response) : Promise<void>
     }
 }
 
+async function moveTodos(req : Request, resp : Response) : Promise<void>
+{
+    /*Handles inter and intra list movement of todo cards */
+
+    try
+    {   
+        const todoItemsMap : Map<string, {src: number,dest:number,destOrder:number,srcOrder:number}> = new Map();
+
+        //Mapping the final state of each changed item
+        const itemChanges : {todoId:string,src:number, dest:number, destOrder:number,srcOrder:number}[] = req.body.itemChanges;
+        itemChanges.forEach((change) => {
+            if(todoItemsMap.has(change.todoId))
+            {
+                const todoItem = todoItemsMap.get(change.todoId);
+                todoItem!.dest = change.dest;
+                todoItem!.destOrder = change.destOrder; 
+            }
+            else
+                todoItemsMap.set(change.todoId, {src : change.src, dest : change.dest, destOrder : change.destOrder, srcOrder :change.srcOrder});
+        });
+
+        //Getting the user collection
+        const userCollection : Collection = await mongodb.db().collection(req.body.userId);
+
+        //Updating the todo items' documents
+        const listCardCountChanges : Map<number,number> = new Map<number,number>();
+        for(const [key,value] of todoItemsMap) 
+        {   
+            //Shifting todo cards
+            if(value.src === value.dest)
+            {
+                if(value.srcOrder < value.destOrder)
+                {
+                    await userCollection.updateMany({$and : [{listCode : value.dest}, {order : {$lte : value.destOrder, $gt : value.srcOrder}}]}, 
+                        {$inc : {order:-1}});
+                }
+                else if(value.srcOrder > value.destOrder)
+                {
+                    await userCollection.updateMany({$and : [{listCode : value.dest}, {order : {$lt: value.srcOrder, $gte: value.destOrder}}]},
+                        {$inc : {order:1}});
+                }
+            }
+            else
+            {
+                await userCollection.updateMany({$and : [{listCode : value.dest}, {order : {$gte : value.destOrder}}]}, {$inc : {order:1}});
+                await userCollection.updateMany({$and : [{listCode : value.src}, {order : {$gt : value.srcOrder}}]}, {$inc : {order:-1}});
+            }
+
+            //Positioning the moved card
+            await userCollection.updateOne({_id : new ObjectId(key)}, {$set : {order:value.destOrder, listCode: value.dest}});
+
+            //Updating the source list card count change
+            if(listCardCountChanges.has(value.src))
+                listCardCountChanges.set(value.src, listCardCountChanges.get(value.src)!-1);
+            else
+                listCardCountChanges.set(value.src, -1);
+
+            //Updating the destination list card count change
+            if(listCardCountChanges.has(value.dest))
+                listCardCountChanges.set(value.dest, listCardCountChanges.get(value.dest)!+1);
+            else
+                listCardCountChanges.set(value.dest, 1);
+
+        };
+
+        //Updating the list card counts
+        const listsDoc = await userCollection.findOne({lastIndex : {$exists : true}}, {projection: {lastIndex:0}});
+        const docId : ObjectId = listsDoc._id; //Getting the id of the document
+        delete listsDoc._id;
+        const setObj : any = {};
+        Object.entries(listsDoc).forEach((list : [string, any]) => {
+            if(listCardCountChanges.has(list[1].code))
+                setObj[`${list[0]}.cards`] = list[1].cards + listCardCountChanges.get(list[1].code);
+        });
+        await userCollection.updateOne({_id : docId}, {$set : setObj});
+
+        resp.sendStatus(200);
+    }
+    catch(err)
+    {
+        console.log(err);
+        resp.sendStatus(200);
+    }
+}
+
 /*****************************Exports*********************/
-export {getUserTodos, createNewList, deleteList, addNewTodo, deleteTodo};
+export {getUserTodos, createNewList, deleteList, addNewTodo, deleteTodo, moveTodos};
+
--- src/Routes/ListApi.ts
+++ src/Routes/ListApi.ts
@@ -2,7 +2,7 @@
 /*****************************Imports*********************/
 import { Router } from "express";
 import { verifyUserToken } from "../Services/Middleware";
-import { getUserTodos, createNewList, deleteList, addNewTodo, deleteTodo } from "../Controllers/ListController";
+import { getUserTodos, createNewList, deleteList, addNewTodo, deleteTodo, moveTodos } from "../Controllers/ListController";
 
 /*****************************Variables*********************/
 const router : Router = Router();
@@ -13,6 +13,7 @@ router.post("/new", verifyUserToken, createNewList);
 router.post("/removelist", verifyUserToken, deleteList);
 router.post("/newTodo", verifyUserToken, addNewTodo);
 router.post("/removetodo", verifyUserToken, deleteTodo);
+router.post("/movetodos", verifyUserToken, moveTodos);
 
 /*****************************Exports*********************/
 export {router as ListApiRouter};
